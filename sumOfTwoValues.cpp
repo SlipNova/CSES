9305a6cdf6a4b886450db8a24ca8b243e67630b4
Sam <sam@Samarths-MacBook-Air.local>
2026-06-04 16:43:44 -0700

    update

---
+++ sumOfTwoValues.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace std::chrono;
+
+using ll = long long;
+using ld = long double;
+
+const int MOD = 1e9+7;
+const ld PI = acos((ld)-1);
+
+template<class T> bool ckmin(T &a, const T &b) {return b < a ? a = b, 1 : 0;}
+template<class T> bool ckmax(T &a, const T &b) {return a < b ? a = b, 1 : 0;}
+
+struct PairHash {
+    size_t operator()(const pair<int,int>& p) const {
+        return hash<long long>()(((long long)p.first << 32) ^ p.second);
+    }
+};
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    ll x;
+    cin >> n >> x;
+    vector<pair<ll,int>> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i].first;
+        a[i].second = i + 1;
+    }
+    sort(a.begin(), a.end());
+    int l = 0, r = n - 1;
+    while (l < r) {
+        ll sum = a[l].first + a[r].first;
+        if (sum == x) {
+            cout << a[l].second << ' ' << a[r].second << '\n';
+            return 0;
+        }
+        else if (sum < x) {
+            l++;
+        }
+        else {
+            r--;
+        }
+    }
+    cout << "IMPOSSIBLE\n";
+}
